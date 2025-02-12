ea2dede48cd28e4c270894e01b4f9d7888565eca
Maxime Rochkoulets <mrochkoulets@gmail.com>
2025-02-12 14:21:58 +0100

    testing for limit

--- test/test_limit.py
+++ test/test_limit.py
@@ -1,97 +1,123 @@
 import unittest
 from decimal import Decimal
-from hypothesis import given, strategies as hstrategies
+from hypothesis import given, strategies as st
+import secrets
 
 from pylob.limit import Limit
 from pylob.enums import OrderSide, OrderStatus
-from pylob.order import OrderParams, BidOrder
+from pylob.order import OrderParams, BidOrder, AskOrder
+from pylob.consts import MIN_VALUE, MAX_VALUE
+from pylob.utils import todecimal
+
+valid_price = st.decimals(min_value=MIN_VALUE, max_value=MAX_VALUE, allow_nan=False, allow_infinity=False)
+valid_side = st.sampled_from(OrderSide)
+valid_order_qty = st.decimals(min_value=MIN_VALUE, max_value=MAX_VALUE, allow_nan=False, allow_infinity=False)
+random_uuid = st.sampled_from([secrets.token_urlsafe(8) for _ in range(1000)])
 
 class TestLimit(unittest.TestCase):
     def setUp(self):
-        self.price = Decimal('100.00')
-        self.side = OrderSide.BID
-        self.limit = Limit(self.price, self.side)
+        pass
         
-    def create_test_order(self, quantity=Decimal('10.00')):
-        order = BidOrder(OrderParams(self.side, self.price, quantity))
-        return order
-
-    @given(price=hstrategies.decimals(min_value='0.0', max_value='1000.00'),
-           side=hstrategies.sampled_from([OrderSide.BID, OrderSide.ASK]))
+    @given(valid_price, valid_side)
     def test_init(self, price, side):
+        price = todecimal(price)
         limit = Limit(price, side)
+
         self.assertEqual(limit.price(), price)
         self.assertEqual(limit.side(), side)
         self.assertEqual(limit.volume(), Decimal(0))
         self.assertEqual(limit.valid_orders(), 0)
 
-    def test_add_order(self):
-        order = self.create_test_order()
-        self.limit.add_order(order)
-        self.assertEqual(self.limit.volume(), Decimal('10.00'))
-        self.assertEqual(self.limit.valid_orders(), 1)
-        self.assertTrue(self.limit.order_exists(order.id()))
-        self.assertEqual(self.limit.get_order(order.id()), order)
-        self.assertEqual(self.limit.get_next_order(), order)
+    def create_order(self, price, side, qty):
+        orderparams = OrderParams(side, price, qty)
+        order = BidOrder(orderparams) if side == OrderSide.BID else AskOrder(orderparams)
+        return order
 
-    def test_order_exists(self):
-        order = self.create_test_order()
-        self.limit.add_order(order)
-        self.assertTrue(self.limit.order_exists(order.id()))
-        self.assertFalse(self.limit.order_exists('1234'))
+    @given(valid_price, valid_side, valid_order_qty)
+    def test_add_order(self, price, side, qty):
+        price = todecimal(price)
+        order = self.create_order(price, side, qty)
+        limit = Limit(price, side)
+        limit.add_order(order)
+        self.assertEqual(limit.volume(), order.quantity())
+        self.assertEqual(limit.valid_orders(), 1)
+        self.assertTrue(limit.order_exists(order.id()))
+        self.assertEqual(limit.get_order(order.id()), order)
+        self.assertEqual(limit.get_next_order(), order)
 
-    def test_get_order(self):
-        order = self.create_test_order()
-        self.limit.add_order(order)
-        retrieved_order = self.limit.get_order(order.id())
+    @given(valid_price, valid_side, valid_order_qty, random_uuid)
+    def test_order_exists(self, price, side, qty, ruuid):
+        price = todecimal(price)
+        order = self.create_order(price, side, qty)
+        limit = Limit(price, side)
+        limit.add_order(order)
+        self.assertTrue(limit.order_exists(order.id()))
+        self.assertFalse(limit.order_exists(ruuid))
+
+    @given(valid_price, valid_side, valid_order_qty)
+    def test_get_order(self, price, side, qty):
+        order = self.create_order(price, side, qty)
+        limit = Limit(price, side)
+        limit.add_order(order)
+        retrieved_order = limit.get_order(order.id())
         self.assertEqual(retrieved_order, order)
 
-    def test_get_next_order(self):
-        order1 = self.create_test_order()
-        order2 = self.create_test_order()
-        self.limit.add_order(order1)
-        self.limit.add_order(order2)
-        self.assertEqual(self.limit.get_next_order(), order1)
+    @given(valid_price, valid_side, valid_order_qty)
+    def test_get_next_order(self, price, side, qty):
+        price = todecimal(price)
+        order1 = self.create_order(price, side, qty)
+        order2 = self.create_order(price, side, qty)
+        limit = Limit(price, side)
+        limit.add_order(order1)
+        limit.add_order(order2)
+        self.assertEqual(limit.get_next_order(), order1)
 
-    def test_delete_next_order(self):
-        order1 = self.create_test_order()
-        order2 = self.create_test_order()
-        self.limit.add_order(order1)
-        self.limit.add_order(order2)
-        self.limit.delete_next_order()
-        self.assertEqual(self.limit.get_next_order(), order2)
-        self.assertEqual(self.limit.volume(), Decimal('10.00'))
-        self.assertEqual(self.limit.valid_orders(), 1)
+    @given(valid_price, valid_side, valid_order_qty, valid_order_qty)
+    def test_delete_next_order(self, price, side, qty1, qty2):
+        price = todecimal(price)
+        order1 = self.create_order(price, side, qty1)
+        order2 = self.create_order(price, side, qty2)
+        limit = Limit(price, side)
+        limit.add_order(order1)
+        limit.add_order(order2)
+        limit.delete_next_order()
+        self.assertEqual(limit.get_next_order(), order2)
+        self.assertEqual(limit.volume(), order2.quantity())
+        self.assertEqual(limit.valid_orders(), 1)
 
-    def test_cancel_order(self):
-        order = self.create_test_order()
-        self.limit.add_order(order)
-        self.limit.cancel_order(order)
+    @given(valid_price, valid_side, valid_order_qty)
+    def test_cancel_order(self, price, side, qty):
+        price = todecimal(price)
+        order = self.create_order(price, side, qty)
+        limit = Limit(price, side)
+        limit.add_order(order)
+        limit.cancel_order(order)
         self.assertEqual(order.status(), OrderStatus.CANCELED)
-        self.assertEqual(self.limit.volume(), Decimal(0))
-        self.assertEqual(self.limit.valid_orders(), 0)
+        self.assertEqual(limit.volume(), Decimal(0))
+        self.assertEqual(limit.valid_orders(), 0)
+        self.assertTrue(limit.empty())
 
-    def test_prune_canceled_orders(self):
-        order1 = self.create_test_order()
-        order2 = self.create_test_order()
-        self.limit.add_order(order1)
-        self.limit.add_order(order2)
+    @given(valid_price, valid_side, valid_order_qty, valid_order_qty)
+    def test_prune_canceled_orders(self, price, side, qty1, qty2):
+        price = todecimal(price)
+        order1 = self.create_order(price, side, qty1)
+        order2 = self.create_order(price, side, qty2)
+        limit = Limit(price, side)
+        limit.add_order(order1)
+        limit.add_order(order2)
         order1.set_status(OrderStatus.CANCELED)
-        self.limit.prune_canceled_orders()
-        self.assertEqual(self.limit.get_next_order(), order2)
-        self.assertEqual(self.limit.volume(), Decimal('10.00'))
-        self.assertEqual(self.limit.valid_orders(), 1)
+        limit.prune_canceled_orders()
+        self.assertEqual(limit.get_next_order(), order2)
+        self.assertEqual(limit.volume(), order2.quantity())
+        self.assertEqual(limit.valid_orders(), 1)
 
-    def test_empty(self):
-        self.assertTrue(self.limit.empty())
-        order = self.create_test_order()
-        self.limit.add_order(order)
-        self.assertFalse(self.limit.empty())
-        self.limit.delete_next_order()
-        self.assertTrue(self.limit.empty())
-
-    def test_repr(self):
-        order = self.create_test_order()
-        self.limit.add_order(order)
-        expected_repr = f"{self.side.name}Limit(price={self.price}, orders=1, volume={Decimal('10.00')})"
-        self.assertEqual(repr(self.limit), expected_repr)
+    @given(valid_price, valid_side, valid_order_qty)
+    def test_empty(self, price, side, qty):
+        price = todecimal(price)
+        limit = Limit(price, side)
+        self.assertTrue(limit.empty())
+        order = self.create_order(price, side, qty)
+        limit.add_order(order)
+        self.assertFalse(limit.empty())
+        limit.delete_next_order()
+        self.assertTrue(limit.empty())
