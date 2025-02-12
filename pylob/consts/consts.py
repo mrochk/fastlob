ea48b065aaade3ff7a25f4456c59c0fbe2b09992
Maxime Rochkoulets <mrochkoulets@gmail.com>
2025-02-12 12:28:52 +0100

    comments in params class

--- pylob/consts/consts.py
+++ pylob/consts/consts.py
@@ -2,12 +2,12 @@ from decimal import Decimal
 from numbers import Number
 from typing import Union
 
-num = Union[Number | Decimal]  # helper typing constant
-
 DEFAULT_DECIMAL_PRECISION: int = 2
 
 MIN_VALUE = UNIT = Decimal('0.01')
 
 MAX_VALUE = Decimal(10e10)
+
+num = Union[Number | Decimal]  # helper typing constant
 
 def zero(): return Decimal('0')
--- pylob/order/params.py
+++ pylob/order/params.py
@@ -4,11 +4,16 @@ from typing import Optional
 
 from pylob.enums import OrderSide, OrderType
 from pylob.utils import num, todecimal
-from pylob.consts import MAX_VALUE
+from pylob.consts import MIN_VALUE, MAX_VALUE
 
 
 @dataclass(repr=True)
 class OrderParams:
+    '''This class is used for instantiating orders, it is necessary because
+    we do not want to have the system to perform any safety checking, or at
+    least it should have to do as few as possible. Therefore this class is 
+    used to force the user to provide valid order parameters.
+    '''
     side: OrderSide
     price: Decimal
     quantity: Decimal
@@ -30,6 +35,10 @@ class OrderParams:
     @staticmethod
     def check_args(side: OrderSide, price: num, quantity: num,
                    type: OrderType, expiry: Optional[float]):
+        '''Check of args correctness. This method is very important, since we
+        do not check for this after the OrderParams object is created for 
+        performance reasons.
+        '''
         if not isinstance(side, OrderSide):
             raise TypeError()
         if not isinstance(price, num):
@@ -41,12 +50,12 @@ class OrderParams:
         if expiry and not isinstance(expiry, float):
             raise TypeError()
 
-        if price <= 0:
-            raise ValueError(f'price ({price}) must be strictly positive')
+        if price < MIN_VALUE:
+            raise ValueError(f'price ({price}) must be greater than 0.01')
 
-        if quantity <= 0:
+        if quantity < MIN_VALUE:
             raise ValueError(
-                f'quantity ({quantity}) must be strictly positive')
+                f'quantity ({quantity}) must be greater than 0.01')
 
         if price > MAX_VALUE:
             raise ValueError(f'price ({price}) is too large')
@@ -55,4 +64,6 @@ class OrderParams:
             raise ValueError(f'quantity ({quantity}) is too large')
 
     def unwrap(self) -> tuple[Decimal, Decimal, OrderType, Optional[float]]:
+        '''Unwrap the params.
+        '''
         return self.price, self.quantity, self.type, self.expiry
